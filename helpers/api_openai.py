2f38b0db148aeb530c505cac70f1df80e8c78e2e
rukayaj@gmail.com <rukayaj@gmail.com>
2023-11-30 13:28:04 +0200

    Apparently not good to make the OpenAI.Client multiple times

--- helpers/api_openai.py
+++ helpers/api_openai.py
@@ -2,17 +2,17 @@ from openai import OpenAI
 import ast
 import json
 
+CLIENT = OpenAI()
 
 def gpt_standardise_text(text, prompt, function):
     openai_args = {
-        'model': 'gpt-4',
+        'model': 'gpt-3.5-turbo-1106',
         'temperature': 0.2,
         'messages': [{'role': 'system', 'content': prompt}, {'role': 'user', 'content': text}],
         'tools': [function],
         'tool_choice': {'type': 'function', 'function': {'name': function['function']['name']}}
     }
-    client = OpenAI()
-    chat_completion = client.chat.completions.create(**openai_args)
+    chat_completion = CLIENT.chat.completions.create(**openai_args)
     fc_args = chat_completion.choices[0].message.tool_calls[0].function.arguments
     try:
         dwc_terms = ast.literal_eval(fc_args)
